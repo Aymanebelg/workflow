2ec40dc5c680041ed0235a6213c63becd8f07c10
mohammed <mohammed.elhaloui.se@gmail.com>
2024-02-18 00:31:23 +0100

    externalize configs into .env

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
+import config from '../config/config'
 
 const connectDB = async (): Promise<void> => {
-  const url = 'mongodb://linkopusUser:linkopus2024@localhost/UserMetadata?authSource=UserMetadata'
   console.log('connecting to MongoDB ...')
   try {
-    await mongoose.connect(process.env.MONGODB_URI ?? url)
+    await mongoose.connect(config.mongodbUri)
     console.log('MongoDB connected')
   } catch (error) {
     console.error('MongoDB connection error:', error)
